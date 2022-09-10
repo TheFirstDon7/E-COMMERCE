06fe15cf33c2c93d1e35160432bae3c410cb0c13
TheFirstDon7 <samaigbiriocasey7@gmail.com>
2022-09-10 09:23:55 +0100

    godu abeg

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ function menutoggle() {
         MenuItems.style.maxHeight = "0px";
     }
 }
+
+var Productimg = document.getElementById("Productimg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function () {
+    Productimg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function () {
+    Productimg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function () {
+    Productimg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function () {
+    Productimg.src = SmallImg[3].src;
+}
